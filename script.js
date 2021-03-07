08ffe7bfbaad3db4407f2597e486027be08e44b2
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-03-07 13:50:06 +0200

    basic

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const gameBoard = document.querySelector(".game-board");
 const gameSize = 12; // later will be dynamic
+let flip = 0;
+const cardsArr = [];
 
 function board() {
 	const cardElement = document.createElement("div");
-	const cardsArr = [];
 	cardElement.classList.add("card");
 	for (let i = 0; i < gameSize; i++) {
 		const cardElement = document.createElement("div");
@@ -33,3 +34,33 @@ function getColors(cardsArr) {
 }
 
 board();
+gameBoard.addEventListener("click", test);
+function test(e) {
+	flip++;
+	const card = e.target;
+	card.setAttribute("data-isFlip", "true");
+	card.style.backgroundColor = card.getAttribute("data-value");
+	if (flip == 2) {
+		match();
+		flip = 0;
+	}
+}
+
+function match() {
+	const flip = cardsArr.filter(
+		(el) => el.getAttribute("data-isFlip") === "true"
+	);
+	if (
+		flip[0].getAttribute("data-value") === flip[1].getAttribute("data-value")
+	) {
+		flip[0].setAttribute("data-isFlip", "true-found");
+		flip[1].setAttribute("data-isFlip", "true-found");
+	} else {
+		setTimeout(function () {
+			flip[0].setAttribute("data-isFlip", "false");
+			flip[0].style.backgroundColor = "purple";
+			flip[1].setAttribute("data-isFlip", "false");
+			flip[1].style.backgroundColor = "purple";
+		}, 1000);
+	}
+}
